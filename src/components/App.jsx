2024362e7d8fe027143bb2b481632b48c9d79d9b
birkyv <90871219+birkyv@users.noreply.github.com>
2022-01-16 16:54:41 -0800

    initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./app.scss";
+
+const App = () => {
+  return (
+    <div className="container">
+      <div className="header">
+        <h1>Welcome to React application</h1>
+        <p>Webpack set up was a success</p>
+      </div>
+    </div>
+  );
+};
+
+export default App;
