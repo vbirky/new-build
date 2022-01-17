325fe5784279a48c4dc63343089ecacabe2e7ba2
Victor Birky <v-victor.b@purple.com>
2022-01-17 12:06:28 -0800

    more refinements

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
         use: ["style-loader", "css-loader", "sass-loader"],
       },
       {
-        test: /\.(woff|woff2|eot|ttf|svg)$/,
+        test: /\.(woff|woff2|eot|ttf|svg|jpg|png)$/,
         use: {
           loader: 'url-loader',
         },
